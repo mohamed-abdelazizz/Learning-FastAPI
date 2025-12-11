4e8b05d307c42a6e2155bea88487d6bd8f41035e
Mohamed Abd El-aziz <mohamed.abdelazizz154@gmail.com>
2025-12-11 19:14:04 +0200

    update api in 9- Nested_Models

--- 9- Nested_Models/api.py
+++ 9- Nested_Models/api.py
@@ -3,11 +3,13 @@ from pydantic import BaseModel, HttpUrl, Field
 
 app = FastAPI()
 
+
 # -----------------------------------
 # Root Endpoint
 # -----------------------------------
 @app.get("/")
 async def read_root():
+    # Returns a simple greeting message
     return {"message": "Hello, World!"}
 
 
@@ -26,7 +28,7 @@ class Item(BaseModel):
     name: str = Field(..., description="Name of the item")
     description: str | None = Field(
         default=None,
-        description="Description of the item"
+        description="Optional description of the item"
     )
     price: float = Field(..., gt=0,
                          description="Price must be greater than zero")
@@ -44,14 +46,15 @@ class Product(BaseModel):
 @app.put("/products/{item_id}")
 async def update_product(
     item_id: int = Path(
-        ..., ge=1,
+        ...,
+        ge=1,
         title="Item ID",
         description="The ID of the item to update. Must be ≥ 1."
     ),
     item: Item = Body(..., description="The updated item data")
 ):
     """
-    Updates an item by ID.
+    Updates an item by ID.\n
     Validates:
     - item_id (must be >= 1)
     - item (validated using Item BaseModel)
@@ -68,14 +71,15 @@ async def update_product(
 @app.post("/products/{product_id}")
 async def create_product(
     product_id: int = Path(
-        ..., ge=1,
+        ...,
+        ge=1,
         title="Product ID",
         description="The product ID. Must be ≥ 1."
     ),
     product: Product = Body(..., description="The full product data")
 ):
     """
-    Creates a new full Product.
+    Creates a new full Product.\n
     Product includes:
     - image (Image Model)
     - item  (Item Model)
